3a9fba87f686a5bb11fa2998a22e2a039485a3d1
Ponyville <favoritePonyville@gmail.com>
2022-06-30 12:24:15 +0400

    add pages/_document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
+import { Head } from 'next/document';
 import { AppProps } from '../node_modules/next/dist/shared/lib/router/router';
+import React from 'react';
 import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }:AppProps):JSX.Element {
   return <Component {...pageProps} />;
