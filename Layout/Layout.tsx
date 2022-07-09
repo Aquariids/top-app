3fe1532e1c2d6683e8cf8ce902e43a17242108f5
Dima kun <FavoritePonyville@gmail.com>
2022-07-09 23:23:19 +0400

    create HOC component withLayout

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { LayoutProps } from "./Layout-props";
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Sidebar from "./Sidebar/Sidebar";
+import { Component, FunctionComponent } from "react";
 
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
@@ -18,6 +19,20 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
             <Footer />
         </>
     );
-}; 
+};
 
-export default Layout;
+
+
+export const withLayout = <T extends Record<string, unknown>>(Сomponent: FunctionComponent<T>) => {
+
+    return function withLayoutComponent(props: T): JSX.Element {
+        return (
+            <Layout>
+                <Сomponent {...props} />
+            </Layout>
+        );
+    };
+};
+
+
+
