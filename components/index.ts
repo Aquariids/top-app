c648981ea7f4ee70962e264045ba5d987d06bd99
Dima kun <FavoritePonyville@gmail.com>
2022-07-09 16:17:03 +0400

    test

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,4 @@ import Htag from './Htag/Htag';
 import Button from './Button/Button';
 import Paragraph from './Paragraph/Paragraph';
 import Tag from './Tag/Tag';
-import Rating from './Rating/Rating';
 export {Htag,Button,Paragraph,Tag,Rating};
