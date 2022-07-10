a3b70816cbf2ecd9302be4e06246ab986d66ffe5
Dima kun <FavoritePonyville@gmail.com>
2022-07-10 15:48:37 +0400

    add plugin date-fns, create grid footer

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,12 +1,16 @@
 import { FooterProps } from "./Footer-props";
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import {format} from 'date-fns';
 
-const Footer = ({ ...props}: FooterProps): JSX.Element => {
+const {footer} = styles;
+const Footer = ({ className,...props}: FooterProps): JSX.Element => {
 
     return (
-        <div {...props}>
-            Footer
+        <div className={cn(className, footer)} {...props}>
+            <span>OwlTop © 2020 - {format(new Date(),'yyyy')} Все права защищены</span>
+            <span>Пользовательское соглашение</span>
+            <span>Политика конфиденциальности</span>
         </div>
     );
 }; 
